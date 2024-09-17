0ca7dbab01ca162e9d0ca5ba66746806a9525bb5
Getmnk <sodafella360@gmail.com>
2024-09-17 20:10:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import pandas as pd
 import streamlit as st
 
 def get_user_inputs():
-    st.header("Heart Disease Prediction Input (TEST, take it with a grain of salt)")
+    st.header("Prever Diabetes (TESTE)")
 
-    age = st.number_input("Age?", min_value=0, max_value=100)
-    sex = st.selectbox("Sex", ["Male", "Female"])
-    chestpain = st.number_input("Chest pain? (0 = none, 1 = bearable, 2 = unbearable, 3 = very painful)", min_value=0, max_value=3, step=1)
-    restingblood = st.number_input("Resting blood pressure?", min_value=0, max_value=200)
-    fastingbloodsg = st.selectbox("Is your fasting blood sugar bigger than 120 mg/dl?", ["No", "Yes"])
-    restingeletro = st.number_input("Resting electrocardiographic results (values 0,1,2)", min_value=0, max_value=2, step=1)
-    heartrate = st.number_input("Maximum heart rate achieved?", min_value=0, max_value=202, step=1)
+    age = st.number_input("Idade?", min_value=0, max_value=100)
+    sex = st.selectbox("Sexo", ["Masculino", "Feminino"])
+    chestpain = st.number_input("Dor no peito? (0 = Nenhuma, 1 = Controlavel, 2 = Irritante, 3 = Muito Dolorido)", min_value=0, max_value=3, step=1)
+    restingblood = st.number_input("Pressão arterial em repouso?", min_value=0, max_value=200)
+    fastingbloodsg = st.selectbox("O seu açúcar no sangue em jejum é maior que 120 mg/dl?", ["No", "Yes"])
+    restingeletro = st.number_input("Resultados eletrocardiográficos em repouso? (valores 0,1,2)", min_value=0, max_value=2, step=1)
+    heartrate = st.number_input("Batimentos cardiacos maximos atingidos?", min_value=0, max_value=202, step=1)
 
     sex = 0 if sex == "Female" else 1
     fastingbloodsg = 0 if fastingbloodsg == "No" else 1
@@ -43,12 +43,12 @@ if st.button("Predict"):
     if len(predictions.shape) == 2:
         if predictions.shape[1] == 1:
             heart_disease_probability = predictions[0][0]
-            st.write(f"Heart Disease Risk Probability: {heart_disease_probability:.2f}")
-            st.write(f"Heart Disease Risk (Binary): {'Yes' if heart_disease_probability > 0.5 else 'No'}")
+            st.write(f"Probabilidade de Doença Cardiaca: {heart_disease_probability:.2f}")
+            st.write(f"Doença Cardiaca (Binario): {'Sim' if heart_disease_probability > 0.5 else 'Não'}")
         else:
-            st.error("Unexpected second dimension size in predictions.")
+            st.error("ERRO! Tamanho inesperado da segunda dimensão nas previsões.")
     elif len(predictions.shape) == 1:
-        st.write(f"Heart Disease Risk (Binary): {'Yes' if predictions[0] == 1 else 'No'}")
+        st.write(f"Doença Cardiaca (Binario): {'Sim' if predictions[0] == 1 else 'Não'}")
     else:
-        st.error("Unexpected shape of prediction output. Check the model and input data.")
+        st.error("ERRO NO MODELO!")
 
